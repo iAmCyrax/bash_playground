26ae5bf5da1dc5fc55df60d6b31ff192de220a9b
Ahmet Gilik <ahmetgilik08@gmail.com>
2022-08-25 20:57:43 +0300

    arithmetic operators finished

---
+++ arithmetic_operations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Arithmetic operators are as same as other languages' operators.
+# +, -, *, /, %
+
+number1=30
+number2=6
+
+# For arithmetic operations, just type $((operation))
+echo $number1 + $number2 # output is `30 + 6`
+echo $((3 + 4))          # output is `36`
--- conditions.sh
+++ conditions.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # Integer Comparisons
-# -eq     Equal
-# -ne     Not Equal
-# -lt     Less Than
-# -le     Less Than Or Equal
-# -gt     Greater Than
-# -ge     Greater Than Or Equal
+# -eq          Equal
+# -ne          Not Equal
+# -lt          Less Than
+# -le          Less Than Or Equal
+# -gt          Greater Than
+# -ge          Greater Than Or Equal
 
 # String Comparisons
-# ==      Equal
-# !=      Not Equal
-# <       Comparison With Alphabetic Position
-# >       Comparison With Alphabetic Position
+# ==           Equal
+# !=           Not Equal
+# <            Comparison With Alphabetic Position
+# >            Comparison With Alphabetic Position
+
+# Operators
+# && (or -a)   And
+# || (or -o)   Or
 
 number=15
 
@@ -30,7 +34,7 @@ if [ $customer_name == "John" ]; then
     echo "Customer's name is John"
 fi
 
-# Example App - Advanced Version Of The "Login Screen App" (read.sh)
+# Example App - Advanced Version Of The "Login Screen App" (in read.sh)
 
 echo "Login Screen"
 echo
@@ -40,7 +44,7 @@ read -rsp "Password: " password
 
 echo
 
-if [ "$username" == "cyrax" ] || [ "$password" == "denx96" ]; then
+if [[ "$username" == "cyrax" && "$password" == "denx96" ]]; then
     echo "Login successful. Welcome!"
 else
     echo "Wrong username or password."
